d116bed3ca77f4ca95656ea44fd57031ebeb3c73
Vlatko Koudela <vlatko@koudela.org>
2020-08-18 18:20:19 +0200

    Excluded hmr from esm and lib folders; 0.4.0-alpha.13

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,33 @@
-module.exports = function(api) {
+module.exports = function (api) {
 	api.cache.never();
 
 	const presets = ['@babel/preset-env', '@babel/preset-react'];
 
-	const plugins = [
-		'react-hot-loader/babel',
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-object-rest-spread',
-		'@babel/plugin-proposal-throw-expressions',
-		[
-			'babel-plugin-styled-components',
-			{
-				pure: true
-			}
-		]
-	];
+	const plugins =
+		process.env.NODE_ENV === 'development'
+			? [
+					'react-hot-loader/babel',
+					'@babel/plugin-proposal-class-properties',
+					'@babel/plugin-proposal-object-rest-spread',
+					'@babel/plugin-proposal-throw-expressions',
+					[
+						'babel-plugin-styled-components',
+						{
+							pure: true
+						}
+					]
+			  ]
+			: [
+					'@babel/plugin-proposal-class-properties',
+					'@babel/plugin-proposal-object-rest-spread',
+					'@babel/plugin-proposal-throw-expressions',
+					[
+						'babel-plugin-styled-components',
+						{
+							pure: true
+						}
+					]
+			  ];
 
 	const env = {
 		cjs: {
